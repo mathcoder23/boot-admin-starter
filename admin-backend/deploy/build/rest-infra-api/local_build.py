dabc6bd32fdf7c2dae6c8d3aab71bc69a6a9f568
Wei Feng <wei.feng@compass.com>
2022-04-03 21:56:20 +0800

    Feat: update
    Feat: update

---
+++ admin-backend/deploy/build/rest-infra-api/local_build.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+import configparser
+import os
+
+_docker_image_name = "boot-admin-starter"
+
+print('docker image name:' + _docker_image_name)
+
+
+def buildImageTag():
+    with open('pom.properties', 'r', encoding='utf-8') as f:
+        content = "[default]\n" + f.read()
+    con = configparser.ConfigParser()
+    con.read_string(content)
+    artifacId = con.get("default", 'artifactId')
+    version = con.get("default", 'version')
+    return "%s_%s" % (artifacId, version)
+
+
+def main():
+    version = buildImageTag()
+    tag = _docker_image_name + version
+    print('docker image tag:', tag)
+
+    print('starting build docker image')
+    os.system('docker build -t %s' % tag)
+    print('finished docker image:', tag)
+
+
+main()
